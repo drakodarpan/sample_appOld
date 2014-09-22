9f99341d0ae1d8f2b878641a7f10b1150240123f
drakodarpan <darko_darpan@prodigy.net.mx>
2014-09-22 23:46:52 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 end
