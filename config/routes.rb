5e2b6fe25542c7fff588e464cd4d62845c7bf520
drakodarpan <darko_darpan@prodigy.net.mx>
2014-09-25 00:42:12 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root :to => 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'contact' => 'static_pages#contact'
-  get 'about' => 'static_pages#about'
-  get 'signup' => 'users#new'
+  get 'sessions/new'
+
+  root :to         => 'static_pages#home'
+  get 'help'       => 'static_pages#help'
+  get 'contact'    => 'static_pages#contact'
+  get 'about'      => 'static_pages#about'
+  get 'signup'     => 'users#new'
+  get 'login'      => 'sessions#new'
+  delete 'logout'  => 'sessions#destroy'
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
